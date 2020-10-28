bd31186fbbf9c0c9cca3e5508e328aa9ca750d8b
Roberto <roberto.cuyar44@gmail.com>
2020-10-28 09:48:16 -0500

    Where exercise completed.

---
+++ where_exercises.sql
@@ -0,0 +1,8 @@
+USE employees;
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE hire_date BETWEEN CAST('1990-01-01' as DATE) AND CAST('1999-12-31' AS DATE) AND birth_date LIKE '%12-25%';
+SELECT * FROM employees WHERE birth_date LIKE '%12-25%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
