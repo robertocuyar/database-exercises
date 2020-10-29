c6afc9a0b0e6f8b4af41511d914876d13ee143ce
Roberto <roberto.cuyar44@gmail.com>
2020-10-29 12:44:16 -0500

    Updated join exercise without complete bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -17,6 +17,18 @@ FROM employees as e
 WHERE to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
+SELECT t.title AS 'Title', COUNT(de.dept_no) AS COUNT
+FROM titles AS t
+JOIN dept_emp de on t.emp_no = de.emp_no
+WHERE t.to_date = '9999-01-01' AND de.dept_no = 'd009'AND de.to_date = '9999-01-01'
+GROUP BY (t.title);
+
+SELECT * FROM dept_emp d
+JOIN employees e on d.emp_no = e.emp_no
+JOIN dept_manager dm on e.emp_no = dm.emp_no
+WHERE dm.dept_no = 'd009'
+ORDER BY dm.to_date DESC;
+
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
        s.salary AS 'Salary'
 FROM employees as e
